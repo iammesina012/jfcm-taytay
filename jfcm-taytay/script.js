33b1669ff2a715875a89c47f30ab8f0e54e9395d
iammesina012 <iammesina012@gmail.com>
2026-01-24 20:59:03 +0800

    Add slideshow and beliefs sections with corresponding images, styles, and footer

--- jfcm-taytay/script.js
+++ jfcm-taytay/script.js
@@ -11,7 +11,39 @@ dropdowns.forEach(currentDropdown => {
       }
     });
 
-    menu.style.display =
+    menu.style.display =  
       menu.style.display === 'block' ? 'none' : 'block';
   });
 });
+
+const slides = [
+    {
+      image: "images/slide1.jpg",
+      text: "We win people to the Lord Jesus Christ through the ministry of evangelism and church planting."
+    },
+    {
+      image: "images/slide2.jpg",
+      text: "We train people to do the work of the ministry through discipleship, ministry and mission training."
+    },
+    {
+      image: "images/slide3.jpg",
+      text: "We send people to do ministry and mission according to their God-given abilities."
+    }
+  ];
+
+  let currentSlide = 0;
+
+  function showSlide(index) {
+    document.getElementById("slide-image").src = slides[index].image;
+    document.getElementById("slide-text").textContent = slides[index].text;
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+  }
